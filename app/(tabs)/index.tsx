db08d690536c3e057b6b3c8b17b7a195a6576947
roritopra <1193102598@u.icesi.edu.co>
2025-05-22 17:09:14 -0500

    feat: initialize movie app with tab navigation and basic screens

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold">Welcome to my movie app!</Text>
+      <Text className="text-2xl font-bold text-dark-100">Welcome to my movie app!</Text>
     </View>
   );
 }
